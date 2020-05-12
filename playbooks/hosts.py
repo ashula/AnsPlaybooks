834ededce661100ebef05e187d75f22126a1e7b3
nutanix <nutanix@localhost.localdomain>
2020-05-12 15:27:51 -0400

    new file:   hosts.py

---
+++ playbooks/hosts.py
@@ -0,0 +1,16 @@
+#! /bin/python
+
+print '{'
+print '  "test2": {'
+print '     "hosts": [',
+print '             "172.16.251.10", ',
+print '             "172.16.251.30", ',
+print '             "172.16.251.17"'
+print '              ]'
+print '           }, '
+print '  "test": {'
+print '     "hosts": [',
+print '             "172.16.251.17" '
+print '              ]'
+print '          } '
+print '}'
